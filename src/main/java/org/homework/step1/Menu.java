5d541efb6b9f7fd318586c71c1b26e880a676cbd
ekd594ff <ekd594ff@gmail.com>
2024-07-09 16:32:09 +0900

    making enum

--- src/main/java/org/homework/step1/Menu.java
+++ src/main/java/org/homework/step1/Menu.java
@@ -1,4 +1,9 @@
 package org.homework.step1;
 
 public enum Menu {
+    NONE(0), ADD(1), DELETE(2), SELECT(3), EXIT(-1);
+    private int value;
+    Menu(int value) {
+        this.value = value;
+    }
 }
